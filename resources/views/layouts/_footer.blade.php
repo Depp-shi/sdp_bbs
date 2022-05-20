07cd5199f1180ac0debcd7cb05044910b4de120a
Depp-shi <946707789@qq.com>
2022-05-20 09:33:21 +0800

    u

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,7 +3,7 @@
     <p class="float-start">
       由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Sdp</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
-
+    <p><a href="https://beian.miit.gov.cn/">浙ICP备18044233号-3</a></p>
     <p class="float-end"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>
